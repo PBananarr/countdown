44d702ca594639a0033a4c2a8fd919675ec31f4d
PBananarr <79418608+PBananarr@users.noreply.github.com>
2025-09-15 10:43:49 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* sw.js — Globaler Service Worker für Birthday-Quest */
 
-const VERSION = "v1.34.";               // <<< bei jedem Release anpassen
+const VERSION = "v1.36.";               // <<< bei jedem Release anpassen
 self.VERSION = VERSION;
 const STATIC_CACHE = `bq-static-${VERSION}`;
 const RUNTIME_CACHE = `bq-runtime-${VERSION}`;
